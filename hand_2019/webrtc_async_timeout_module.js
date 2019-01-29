7874bb1e1eaae0460117f124ab72ff492d922b1c
Ganeko <ganeko@griffin.local>
2019-01-29 18:15:54 +0900

    try no STUN

--- hand_2019/webrtc_async_timeout_module.js
+++ hand_2019/webrtc_async_timeout_module.js
@@ -118,7 +118,8 @@ const TIMEOUT_FOR_VANILLA = 2000;
 
 // WebRTCを利用する準備をする
 function prepareNewConnection() {
-  const pc_config = {"iceServers":[ {"urls":"stun:stun.webrtc.ecl.ntt.com:3478"} ]};
+  //const pc_config = {"iceServers":[ {"urls":"stun:stun.webrtc.ecl.ntt.com:3478"} ]}; // Cause long time for gathering ICE candidate in some network
+  const pc_config = {"iceServers":[]};
   const peer = new RTCPeerConnection(pc_config);
 
   // リモートのMediStreamTrackを受信した時
